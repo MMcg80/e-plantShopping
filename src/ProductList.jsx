ef638b0abfb07760813bc2b95b67bbe202feba83
MMcg80 <shirokuma025@gmail.com>
2026-01-14 09:39:57 -0500

    cart qty display

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -270,7 +270,7 @@ function ProductList({ onHomeClick }) {
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
-        setCartCount((prevCount) => prevCount + 1);
+        
       };
     return (
         <div>
